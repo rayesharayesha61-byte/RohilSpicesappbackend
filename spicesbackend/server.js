fa06acefbfbaa46ee3460fcf450b24d815481dce
rayesharayesha61-byte <rayesharayesha61@gmail.com>
2026-05-12 12:55:55 +0530

    backend setup

--- spicesbackend/server.js
+++ spicesbackend/server.js
@@ -810,8 +810,6 @@ app.get("/api", (req, res) => {
   res.send("API Working ✅");
 });
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, "0.0.0.0", () => {
-  console.log("🚀 Server running on port", PORT);
+app.listen(5000, "0.0.0.0", () => {
+  console.log("🚀 Server running on port 5000");
 });
